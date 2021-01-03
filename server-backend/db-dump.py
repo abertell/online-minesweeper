9e5e87a1c5c4e17a63765a9284bcc1549a4924b1
dnialh <54636440+dnialh@users.noreply.github.com>
2021-01-03 03:22:08 -0500

    Update db-dump.py

--- server-backend/db-dump.py
+++ server-backend/db-dump.py
@@ -3,10 +3,10 @@ import sqlite3 as sl
 database = 'minesweeper.db'
 con = sl.connect(database)
 
-for table in ('GAMES','PERFORMANCE_TOP','PERFORMANCE_TOP_10','PERFORMANCE_PP','USER'):
+for table in ('GAMES','PERFORMANCE_TOP','PERFORMANCE_TOP_10','PERFORMANCE_PP','PERFORMANCE_ALL','USER'):
     print(table)
     with con:
-        data = con.execute(f"SELECT * FROM {table}")
+        data = con.execute(f"SELECT * FROM {table}")    
         for row in data:
             print(row)
     print()
