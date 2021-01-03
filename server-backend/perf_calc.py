a0e7be08872255943f2df0d37908eae836d6e745
dnialh <54636440+dnialh@users.noreply.github.com>
2021-01-03 03:24:26 -0500

    Update perf_calc.py

--- server-backend/perf_calc.py
+++ server-backend/perf_calc.py
@@ -1,4 +1,9 @@
 import sqlite3 as sl
+from math import log
+
+def pp(m,w,h,x):
+    a = w * h
+    return ((4.22*log(106+a)-16.8)**(m/a)-1)*100*(0.5)**((a-m-8)/(x-8)-1)
 
 database = 'minesweeper.db'
 
@@ -7,7 +12,7 @@ sizes = [(8,8),(9,9),(16,16),(16,30),(24,30)]
 def perf_add(user):
     con = sl.connect(database)
     
-    for table in ('PERFORMANCE_TOP', 'PERFORMANCE_TOP_10', 'PERFORMANCE_PP'):
+    for table in ('PERFORMANCE_TOP', 'PERFORMANCE_TOP_10', 'PERFORMANCE_PP', 'PERFORMANCE_ALL'):
         request = f"INSERT INTO {table} (name) VALUES ('{user}')"
         con.execute(request)
 
@@ -24,8 +29,8 @@ def perf_calc(user):
     
     for width, height in sizes:
         with con:
-            request = f"SELECT score FROM GAMES WHERE NAME = '{user}' AND (WIDTH = {width} AND HEIGHT = {height} OR WIDTH = {height} AND HEIGHT = {width})"
-            data = list(map(lambda x: int(x[0]),con.execute(request)))
+            request = f"SELECT score FROM GAMES WHERE NAME = ? AND (WIDTH = {width} AND HEIGHT = {height} OR WIDTH = {height} AND HEIGHT = {width})"
+            data = list(map(lambda x: int(x[0]),con.execute(request,[user])))
             data.sort()
 
             top.append(data[-1] if data else 0)
@@ -50,10 +55,44 @@ def perf_calc(user):
         request = f'''UPDATE {table}
                     SET
                     {t_string}
-                    WHERE NAME = "{user}"'''
-
+                    WHERE NAME = ?'''
         with con:
-            con.execute(request)
+            con.execute(request,[user])
+
+    with con:
+        request = f"SELECT score FROM GAMES WHERE NAME = ? AND (WIDTH >= 8 AND HEIGHT >= 8)"
+        data = list(map(lambda x: int(x[0]),con.execute(request,[user])))
+        data.sort()
+
+    pp_all = 0
+    for v in data:
+        pp_all *= .95
+        pp_all += v
+
+    request = 'UPDATE PERFORMANCE_ALL SET pp = ? WHERE name = ?'
+    with con:
+        con.execute(request, (pp_all, user))
+            
     con.commit()
     con.close()
+
+def pp_recalc():
+    con = sl.connect(database)
+
+    users = set()
+
+    data = con.execute(f"SELECT * FROM GAMES")
+    upd = []
+
+    
+    for row in GAMES:
+        print(row)
+
+    
+
+    con.commit()
+    con.close()
+    
+
+    
     
