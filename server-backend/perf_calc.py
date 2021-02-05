fe1a5d4e83a5772f2e980508f688151b6864ee64
abertell <59854865+abertell@users.noreply.github.com>
2021-02-05 06:28:56 -0500

    nerf incomplete large slightly more

--- server-backend/perf_calc.py
+++ server-backend/perf_calc.py
@@ -14,8 +14,8 @@ def pp(m,w,h,x):
     base=lambda m,a:line(a)**ramp(m,a)-1
     ecc=lambda w,h:max(w/h,h/w)**.05
     cutoff=lambda m,a,x:(1-.1/(1+log(a/64)))**(x<a-m)
-    dropoff=lambda m,a,x:.5**(log(a)*((a-m-8)/(x-8)-1))
-    large=lambda a:(100-(log(1+log(max(a,1000))/log(1000),2)-1)*60)/100
+    dropoff=lambda m,a,x:.3**(log(a)*((a-m-8)/(x-8)-1))
+    large=lambda a:(100-(log(1+log(max(a,1000))/log(1000),2)-1)*80)/100
     
     return norm*base(m,w*h)*ecc(w,h)*cutoff(m,w*h,x)*dropoff(m,w*h,x)*large(w*h)
 
