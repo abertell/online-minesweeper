8e68c3b11f8740da57b55f5daa39de777905e7eb
abertell <59854865+abertell@users.noreply.github.com>
2021-02-08 22:26:54 -0500

    nerf dropoff

--- server-backend/perf_calc.py
+++ server-backend/perf_calc.py
@@ -15,7 +15,7 @@ def pp(m,w,h,x):
     ecc=lambda w,h:max(w/h,h/w)**.05
     cutoff=lambda m,a,x:(1-.1/(1+log(a/64)))**(x<a-m)
     balance=lambda r:max(1-100*(1-r)**2,r*r)
-    dropoff=lambda m,a,x:.35**(log(a)*(1/balance((x-8)/(a-m-8))-1))
+    dropoff=lambda m,a,x:.4**(log(a)*(1/balance((x-8)/(a-m-8))-1))
     large=lambda a:(100-(log(1+log(max(a,1000))/log(1000),2)-1)*75)/100
     
     return norm*base(m,w*h)*ecc(w,h)*cutoff(m,w*h,x)*dropoff(m,w*h,x)*large(w*h)
