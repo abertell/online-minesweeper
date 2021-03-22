9ee971c5fc980194abbcb2c4c7c18de87f401a1e
abertell <59854865+abertell@users.noreply.github.com>
2021-03-22 04:17:50 -0400

    Add files via upload

---
+++ server-backend/bot_api.py
@@ -0,0 +1,67 @@
+import asyncio, websockets
+from collections import deque
+
+# Modifiable parameters
+user = "BOT|example_name"
+pwd = "example_pwd"
+size_x = 30
+size_y = 24
+mines = 180
+disp_game = False
+disp_results = True
+games = 1
+
+queue = deque()
+
+import random
+async def bot(board, x, y, m):
+    global queue
+    
+    # Example Bot
+    unknown = []
+    for x in range(size_x):
+        for y in range(size_y):
+            if board[y][x] == '-1':
+                unknown.append((x,y))
+    move = random.choice(unknown)
+
+    queue.append(move)
+
+async def play():
+    global user, pwd, size_x, size_y, mines
+    global disp_game, disp_results, queue, games
+    server = ""
+    async with websockets.connect(server) as server:
+        await server.send(f'RENAME|{user} {pwd}')
+        data = await server.recv()
+        while games:
+            games -= 1
+            await server.send(f'CREATE_ROOM|{size_x} {size_y} {mines}')
+            data = await server.recv()
+            room_id = data.split()[1]
+            await server.send(f'JOIN_ROOM|{room_id}')
+            data = await server.recv()
+            _, _, x, y, m, sc, stat, lb_len, *rest = data.split()
+            x, y, m = map(int,(x, y, m))
+            while stat == "1":
+                c = 4*int(lb_len)
+                lb = rest[:c]
+                board = []
+                for i in range(y):
+                    board.append(rest[c+i*x:c+(i+1)*x])
+                if disp_game:
+                    print()
+                    for i in board:
+                        print(' '.join(i))
+                    print()
+                await bot(board, x, y, m)
+                while queue:
+                    x, y = queue.popleft()
+                    await server.send(f'MOVE|{x} {y}')
+                    data = await server.recv()
+                _, _, x, y, m, sc, stat, lb_len, *rest = data.split()
+                x, y, m = map(int,(x, y, m))
+            if disp_results:
+                print(f'Score: {sc}/{x*y-m}')
+
+asyncio.get_event_loop().run_until_complete(play())
